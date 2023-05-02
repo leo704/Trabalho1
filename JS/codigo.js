cf98139d2875fee826d817a0dc96ea0c354236d1
leo704 <84240124+leo704@users.noreply.github.com>
2023-05-02 16:21:41 -0300

    Continuam as implementações

--- JS/codigo.js
+++ JS/codigo.js
@@ -1,1 +1,13 @@
 
+//var fm=document.forms.item(0).id;
+//var fm=document.forms[0].id;
+//var fm=document.getElementById(formulario).elements[1].value;
+var fm=document.getElementById("formulario");
+var el=fm.elements;
+
+
+
+
+function recebeBtn() {
+    
+}
