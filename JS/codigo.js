524909ccaae23ee9e3156e24db238c718c960083
leo704 <84240124+leo704@users.noreply.github.com>
2023-06-08 16:17:33 -0300

    nada nao

--- JS/codigo.js
+++ JS/codigo.js
@@ -1,13 +1,55 @@
+console.log("hello");
+/*
+let usuario = {
+    nome,
+    telefone,
+    sexo,
+    ofertas
+};*/
 
-//var fm=document.forms.item(0).id;
-//var fm=document.forms[0].id;
-//var fm=document.getElementById(formulario).elements[1].value;
-var fm=document.getElementById("formulario");
-var el=fm.elements;
+let user = {
+  name: "John",
+  age: 30,
+};
 
+console.log(user.name);
 
+let produtos = [
+  {
+    id: "1xd32",
+    tipo: "Computador",
+    fabricante: "Dell",
+    memoria: "8Gb",
+    peso: "1.2kg",
+    importado: false,
+  },
+  {
+    id: "1s42",
+    tipo: "Mouse",
+    fabricante: "C3",
+    peso: "100g",
+    importado: true,
+  },
+  {
+    id: "dz85x",
+    tipo: "Teclado",
+    fabricante: "Microsoft",
+    peso: "250g",
+    importado: true,
+  },
+];
 
-
-function recebeBtn() {
-    
+for (let i = 0; i < produtos.length; i++) {
+  console.log(produtos[i]); //cuidado
 }
+produtos.forEach((p) => {
+  p.fabricante = p.fabricante.toUpperCase();
+  console.log(p);
+});
+produtos.forEach((item) => console.log(item.fabricante));
+console.log("------------------");
+
+let fabricantes = produtos.map((p) => {
+  return p.fabricante;
+});
+console.log(fabricantes);
